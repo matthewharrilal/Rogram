3419866723015b901f8f893fd417262d99709957
Space Wizard <spacewizardmoneygang@Spaces-MBP.lan>
2024-08-25 11:30:30 -0400

    Injecting albums view controller with service dependencies

--- Rogram/SceneDelegate.swift
+++ Rogram/SceneDelegate.swift
@@ -17,6 +17,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        let networkServiceImplementation = NetworkServiceImplementation()
+        let postFetcherImplementation = PostFetcherImplementation(networkService: networkServiceImplementation)
+        window?.rootViewController = AlbumsViewController(postService: postFetcherImplementation)
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- Rogram/View Controllers/ViewController.swift
+++ Rogram/View Controllers/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  AlbumsViewController.swift
 //  Rogram
 //
 //  Created by Space Wizard on 8/25/24.
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class AlbumsViewController: UIViewController {
+    
+    private let postService: PostFetcherProtocol
+    
+    init(postService: PostFetcherProtocol) {
+        self.postService = postService
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        Task {
+            await postService.fetchPostObjects()
+        }
     }
-
-
 }
 
