bd29e595d4f77fbade2157df8b81903ba9360844
Space Wizard <spacewizardmoneygang@Spaces-MBP.lan>
2024-08-25 19:09:02 -0400

    Added implementation for the detail presentation controller and the custom transition delegate

--- Rogram/Transitions/PhotoDetailPresentationController.swift
+++ Rogram/Transitions/PhotoDetailPresentationController.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import UIKit
+
+// MARK: TODO -> Add Documentation
+class PhotoDetailPresentationController: UIPresentationController {
+    
+    override init(presentedViewController: UIViewController, presenting presentingViewController: UIViewController?) {
+        super.init(presentedViewController: presentedViewController, presenting: presentingViewController)
+    }
+    
+    override var frameOfPresentedViewInContainerView: CGRect {
+        UIScreen.main.bounds
+    }
+}
--- Rogram/Transitions/PhotoTransitionDelegate.swift
+++ Rogram/Transitions/PhotoTransitionDelegate.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import UIKit
+
+// MARK: TODO -> Add Documentation
+class PhotoTransitionDelegate: NSObject, UIViewControllerTransitioningDelegate {
+    
+    private lazy var transitionAnimator = PhotoDetailAnimator(startingFrame: startingFrame)
+    
+    public var startingFrame: CGRect = .zero {
+        didSet {
+            transitionAnimator = PhotoDetailAnimator(startingFrame: startingFrame)
+        }
+    }
+    
+    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
+        let presentationController = PhotoDetailPresentationController(
+            presentedViewController: presented,
+            presenting: presenting
+        )
+        
+        return presentationController
+    }
+    
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transitionAnimator.transitioningForward = true
+        return transitionAnimator
+    }
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transitionAnimator.transitioningForward = false
+        return transitionAnimator
+    }
+}
