48b3991da2f7d19c14479c872f58658670ded0de
Space Wizard <spacewizardmoneygang@Spaces-MBP.lan>
2024-08-25 12:03:42 -0400

    Added model for Aggregated post with combined image and title

---
+++ Rogram/Models/AggregatedPost.swift
@@ -0,0 +1,14 @@
+//
+//  AggregatedPost.swift
+//  Rogram
+//
+//  Created by Space Wizard on 8/25/24.
+//
+
+import Foundation
+import UIKit
+
+struct AggregatedPost {
+    let image: UIImage
+    let title: String
+}
