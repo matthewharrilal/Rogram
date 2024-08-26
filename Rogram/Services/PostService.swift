bc6d9e53e269625518f46bcb029548230b76c00f
Space Wizard <spacewizardmoneygang@Spaces-MBP.lan>
2024-08-26 01:07:04 -0400

    Removing album fetching related logic outside of Post Service into it's own service

--- Rogram/Services/PostService.swift
+++ Rogram/Services/PostService.swift
@@ -10,7 +10,6 @@ import Foundation
 protocol PostFetcherProtocol: AnyObject {
     func fetchPostObjects() async -> [Post]?
     func streamAggregatedPosts(posts: [Post]?) async -> AsyncStream<AggregatedPost>?
-    func fetchAlbumCollections() async -> AsyncStream<[Post]?>
 }
 
 class PostFetcherImplementation: PostFetcherProtocol {
@@ -30,31 +29,7 @@ class PostFetcherImplementation: PostFetcherProtocol {
     func fetchPostObjects() async -> [Post]? {
         return await networkService.executeRequest(urlString: Constants.urlString)
     }
-    
-    // MARK: TODO -> Clean up this method
-    func fetchAlbumCollections() async -> AsyncStream<[Post]?> {
-        AsyncStream<[Post]?> { [weak self] continuation in
-            guard let self = self else { return }
-            Task {
-                await withTaskGroup(of: [Post]?.self) { taskGroup in
-                    for counter in 0..<10 {
-                        taskGroup.addTask {
-                            let urlString = "https://jsonplaceholder.typicode.com/album/\(counter)/photos"
-                            print(urlString)
-                            return await self.networkService.executeRequest(urlString: urlString)
-                        }
-                    }
-                    
-                    for await albumCollection in taskGroup {
-                        continuation.yield(albumCollection)
-                    }
-                    
-                    continuation.finish()
-                }
-            }
-        }
-    }
-    
+
     // MARK: TODO -> Clean up this method
     func streamAggregatedPosts(posts: [Post]?) async -> AsyncStream<AggregatedPost>? {
         // MARK: TODO -> Throw errors
